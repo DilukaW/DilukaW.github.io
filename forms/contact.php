cfba019853898f27c5346985520eb4f875173a82
Diluka Wijesinghe <92380086+DilukaW@users.noreply.github.com>
2022-06-14 10:53:22 +0530

    Finalized

--- forms/contact.php
+++ forms/contact.php
@@ -19,7 +19,7 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
   $contact->smtp = array(
-    'host' => 'example.com',
+    'host' => '',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
